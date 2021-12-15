c1b40de625d60bdffb235ba7a80ada58ab0347ae
enocPierre <enoc.pierre@mercadolivre.com>
2021-12-15 14:41:36 -0300

    catgories entity creted

--- src/main/java/com/example/catalog/domain/entity/Category.java
+++ src/main/java/com/example/catalog/domain/entity/Category.java
@@ -1,5 +1,6 @@
 package com.example.catalog.domain.entity;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public class Category {
@@ -9,5 +10,49 @@ public class Category {
 
     public Category() {
 
+    }
+
+    public Category(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null) throw new IllegalArgumentException("");
+        if (name.length() == 0) throw  new IllegalArgumentException("");
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category)) return false;
+        Category category = (Category) o;
+        return id.equals(category.id) && Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
