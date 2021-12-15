74e6122096503ed5d7481f4614545aab62e1e043
pierreEnoc <pierre_enoc@yahoo.fr>
2021-12-15 14:29:55 -0300

    start project with strure base

---
+++ src/main/java/com/example/catalog/domain/entity/Category.java
@@ -0,0 +1,13 @@
+package com.example.catalog.domain.entity;
+
+import java.util.UUID;
+
+public class Category {
+
+    private UUID id;
+    private String name;
+
+    public Category() {
+
+    }
+}
